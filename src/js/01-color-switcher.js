8c942497e16747551a2f881b008d7a3c2b57d5a7
Kapytsia Arthur <marlboro2224@gmail.com>
2023-10-14 15:58:11 +0300

    add collor switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonsEl = document.querySelectorAll('button');
+const buttonStart = buttonsEl[0];
+const buttonStop = buttonsEl[1];
 
+let intervalDicrimentId;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function getRandomBackColor() {
+  document.body.style.background = getRandomHexColor();
+}
+
+buttonStart.addEventListener('click', () => {
+  intervalDicrimentId = setInterval(getRandomBackColor, 1000, 1000);
+  buttonStart.disabled = true;
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(intervalDicrimentId);
+});
