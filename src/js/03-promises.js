d99125be6c794a8ddec35e764d91e929a8507c53
Kapytsia Arthur <marlboro2224@gmail.com>
2023-10-26 04:03:54 +0300

    add promise fn

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const formEL = document.querySelector('.form');
+
+formEL.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let firstDelayValue = parseInt(
+    document.querySelector('input[name="delay"]').value
+  );
+  const delayStepValue = parseInt(
+    document.querySelector('input[name="step"]').value
+  );
+  const amountValue = parseInt(
+    document.querySelector('input[name="amount"]').value
+  );
+
+  for (let i = 1; i <= amountValue; i++) {
+    createPromise(i, firstDelayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Fulfilled promise ${position} in ${delay}ms`);
+      });
+    firstDelayValue += delayStepValue;
+  };
+});
